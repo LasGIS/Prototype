3ba5d54f4ea4bdfeb61b3ac03ec361fd3d63ae6b
Vladimir Laskin <VLaskin@luxoft.com>
2020-06-19 13:40:23 +0600

    Kotlin: add Swagger configuration

---
+++ kotlin_spring_mybatis/src/main/kotlin/com/lasgis/kotlin/web/rest/SwaggerConfiguration.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020. Prototype
+ */
+
+package com.lasgis.kotlin.web.rest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+/**
+ * <description>
+ *
+ * @author VLaskin
+ * @since <pre>19.06.2020</pre>
+ */
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+
+    @Bean
+    open fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+}
